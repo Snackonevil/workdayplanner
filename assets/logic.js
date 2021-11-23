dedbf4b1d75ac88cbf89eb77115afa061a676a77
Kevin Lacson <lacsonky@gmail.com>
2021-11-22 20:19:39 -0500

    trying out logic. added event slots

--- assets/logic.js
+++ assets/logic.js
@@ -1,8 +1,24 @@
 var todaysDate = $('#todaysDate');
+var currHour = moment().format('h');
 
 todaysDate.text(moment().format('dddd, MMMM D, YYYY, h:mm a'));
 
-var hour1Btn = $('.row .btn');
+
+// var hour1Btn = $('.row .btn');
+
+
+const hour1Btn = $('#hour-1 .btn');
+const hour2 = $('#hour-2');
+const hour3 = $('#hour-3');
+const hour4 = $('#hour-4');
+const hour5 = $('#hour-5');
+const hour6 = $('#hour-6');
+const hour7 = $('#hour-7');
+const hour8 = $('#hour-8');
+
+let colorCode = () => {
+
+}
 
 hour1Btn.click((e) => {
 
@@ -13,5 +29,15 @@ hour1Btn.click((e) => {
     } else {
         icon.removeClass('bi-save');
         icon.addClass('bi-gear');
-    } 
+    }
 })
+
+
+// moment(moment().format('h')).isBefore('8')
+
+// $('.btn').each(() => {
+//     if (moment(moment().format('h')).isBefore('8')) {
+//         console.log('cool')
+//     }
+//     }
+// )
