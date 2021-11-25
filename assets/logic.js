677a001d2be34e2d07fa929a326dfee12f760a91
Kevin Lacson <lacsonky@gmail.com>
2021-11-24 22:16:46 -0500

    tested moment.js with color classes

--- assets/logic.js
+++ assets/logic.js
@@ -1,12 +1,12 @@
-$(document).ready(loadPage)
-var planner = $('#planner')
+$(document).ready(loadPage);
+
+var planner = $('#planner');
 
 var currHour = moment().format('H');
-var today = moment().format('YYYY-MM-DD')
-var hourNow = moment().format(`${today} ${currHour}`)
-$('#todaysDate').text(moment().format('dddd, MMMM D, YYYY, h:mm a'));
+var today = moment().format('YYYY-MM-DD');
+var hourNow = moment().format(`${today} ${currHour}`);
 
-var todaytest = moment().format('YYYY-MM-DD H')
+$('#todaysDate').text(moment().format('dddd, MMMM D, YYYY, h:mm a'));
 
 let schedule = [
     {
@@ -54,9 +54,7 @@ let schedule = [
         "time": "4:00pm",
         "event": ""
     }
-]
-
-
+];
 
 function loadPage () {
     var output = ''
@@ -64,87 +62,48 @@ function loadPage () {
     if (events == null) {
         localStorage.setItem('events', JSON.stringify(schedule))
         var events = JSON.parse(localStorage.getItem('events'))
-    } 
+    }
+
     events.forEach((event, i) => {
         var colorClass = ''
         if (moment(hourNow).isBefore(`${today} ${event.hour}`, 'hour') === false) {
             colorClass = 'past'
-        } else if (moment(hourNow).isSame(`${today} ${event.hour}`, 'hour') === true){
-            hour = 'now'
         } else {
             colorClass = 'future'
         }
-        // console.log(event)
-        // console.log(i)
+        if (moment(hourNow).isSame(`${today} ${event.hour}`, 'hour') === true){
+            colorClass = 'now'
+        }
+
          output +=
-        `<div  class="row mb-1 border-bottom">
+        `<div class="row mb-1 border-bottom">
             <div class="col-lg-1 p-3 align-middle text-end">${event.time}</div>
-            <textarea id="${i}" data-slot="${i}" class="col-lg-10 ${colorClass}" disabled>${event.event}</textarea>
+            <textarea id="${i}" class="col-lg-10 ${colorClass}" disabled>${event.event}</textarea>
             <button type="button" data-toggle="button" aria-pressed="false" autocomplete="off" class="bi bi-gear col-lg-1 btn btn-outline-primary p-3"></button>
         </div>`
     })
-    // console.log(output)
+
     planner.html(output)
 
-}
-
-function savePage () {
-    var save = JSON.stringify(events)
-    localStorage.setItem('events', save)
-}
+};
 
 // If click ? unlock textarea and change icon : lock text area and save content
 $('.container').on('click', ('button'), (e) => {
     var events = JSON.parse(localStorage.getItem('events'))
     var icon = $(e.currentTarget)
-    var textArea = icon.siblings()[1]
-    var slot = textArea.dataset.slot
-    var stuff = $(`#${slot}`)
+    var slot = icon.siblings()[1].id
+    var task = $(`#${slot}`)
 
-    
     if (icon.attr( 'aria-pressed' ) === 'false') {
         icon.removeClass('bi-gear')
         icon.addClass('bi-save')
         icon.siblings().removeAttr('disabled')
-
     } else {
-        events[slot].event = stuff.val()
-        localStorage.setItem('events', JSON.stringify(events))
+        events[slot].event = task.val();
+        localStorage.setItem('events', JSON.stringify(events));
         icon.removeClass('bi-save');
         icon.addClass('bi-gear');
-        icon.siblings().attr('disabled', '')
+        icon.siblings().attr('disabled', '');
     }
 
-    
-})
-
-// hour1Btn.click((e) => {
-// // console.log(e.target)
-//     var icon = $(e.currentTarget)
-//     if (hour1Btn.attr( 'aria-pressed' ) === 'false') {
-//         icon.removeClass('bi-gear')
-//         icon.addClass('bi-save')
-//         $('#hour-1 textarea').removeAttr('disabled')
-//     } else {
-//         icon.removeClass('bi-save');
-//         icon.addClass('bi-gear');
-//         $('#hour-1 textarea').attr('disabled', '')
-//     }
-//     console.log(icon)
-// });
-
-// hour2Btn.click((e) => {
-
-//     var icon = $(e.currentTarget)
-//     if (hour2Btn.attr( 'aria-pressed' ) === 'false') {
-//         icon.removeClass('bi-gear')
-//         icon.addClass('bi-save')
-//     } else {
-//         icon.removeClass('bi-save');
-//         icon.addClass('bi-gear');
-//     }
-
-// });
-
-
-// moment(moment().format('h')).isBefore('8')
+});
