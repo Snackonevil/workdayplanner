b08b190381fac09284a95ba5c82c0cf6a725476e
Kevin Lacson <lacsonky@gmail.com>
2021-12-01 17:51:31 -0500

    added comments to logic.js

--- assets/logic.js
+++ assets/logic.js
@@ -1,9 +1,10 @@
 $(document).ready(loadPage);
 
-// Main area
+// Main area for hour slots
 const planner = $('#planner');
 
-// Variables for moment.js formats to compare time
+// Variables for moment.js formats to compare time 
+// (see lines 72 - 84)
 var currHour = moment().format('H');
 var today = moment().format('YYYY-MM-DD');
 var hourNow = moment().format(`${today} ${currHour}`);
@@ -11,12 +12,12 @@ var hourNow = moment().format(`${today} ${currHour}`);
 // Current time in header
 $('#todaysDate').text(moment().format('dddd, MMMM D, YYYY, h:mm a'));
 
-// Storage for hour slots
+// Array of hour slots to be stored
 let schedule = [
     {
         "hour": "08", // Hour for moment.js
         "time": "8:00am", // Label for slot
-        "event": "" // Text in event slot
+        "event": "" // Text of event in slot
     },
     {
         "hour": "09",
@@ -60,21 +61,24 @@ let schedule = [
     }
 ];
 
-// Load the hour slots onto the page
+// Load the hour slots onto the page;
 function loadPage () {
-    var output = '';
+    // Empty variable to store string interpolation;
+    var output = ''; 
+    // Pull and parse 'events' array;
     var events = JSON.parse(localStorage.getItem('events'));
+    // If nothing currently in local storage, store empty schedule array and pull from storage as 'events';
     if (events == null) {
         localStorage.setItem('events', JSON.stringify(schedule));
         var events = JSON.parse(localStorage.getItem('events'));
     }
 
-    // Iterate through array of hour slots
+    // Iterate through array of hour slots;
     events.forEach((event, i) => {
         
-        // Determine color of hour slot based on time
+        // Determine color of hour slot based on time;
         var colorClass = ''
-        if (moment(hourNow).isBefore(`${ today} ${event.hour}`, 'hour') === true) {
+        if (moment(hourNow).isBefore(`${today} ${event.hour}`, 'hour') === true) {
             colorClass = 'future'
         } else {
             colorClass = 'past'
@@ -83,7 +87,7 @@ function loadPage () {
             colorClass = 'now'
         }
         
-        // Populate output variable with HTML and template literals
+        // Populate output variable with HTML using string interpolation and template literals;
          output +=
         `<div class="row mb-1 border-bottom">
             <div class="col-lg-1 p-3 align-middle text-end">${event.time}</div>
@@ -92,31 +96,31 @@ function loadPage () {
         </div>`
     })
 
-    // Write output variable into planner div
+    // Write output variable into planner div;
     planner.html(output)
 
 };
 
-// If button is clicked ? unlock textarea and change icon 
-//      : lock text area and save content
+// If button is clicked ? unlock textarea and change icon
+//      : lock text area and save content;
 $('.container').on('click', ('button'), (e) => { 
-    // Retrieve data from local storage and store in variable
+    // Retrieve data from local storage and store in variable;
     var events = JSON.parse(localStorage.getItem('events'))
-    // Stores current target if it's a button in the planner div
+    // Stores current target if it's a button in the planner div;
     var icon = $(e.currentTarget)
-    // Stores textarea ID (index 1 in sibling array of target)
+    // Stores textarea ID (index 1 in sibling array of target);
     var slot = icon.siblings()[1].id 
-    // Stores template literal to JQuery ID selector
+    // Stores template literal to JQuery ID selector;
     var task = $(`#${slot}`) 
 
-    // Change icon in button and enable textarea
+    // Change icon in button and enable textarea;
     if (icon.attr( 'aria-pressed' ) === 'false') {
         icon.removeClass('bi-gear')
         icon.addClass('bi-save')
         icon.siblings().removeAttr('disabled')
     } else {
-        // Change icon in button, add event text to corresponding slot, and disable textarea
-        // Save entry to local storage
+        // Change icon in button, add event text to corresponding slot, and disable textarea;
+        // Save entry to local storage;
         events[slot].event = task.val();
         localStorage.setItem('events', JSON.stringify(events));
         icon.removeClass('bi-save');
